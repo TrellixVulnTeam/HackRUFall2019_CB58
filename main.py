9bf81c7cf331192ed69a840336e947bf91b9ce89
vishal <vishal.patel929@gmail.com>
2019-10-19 14:32:47 -0400

    Just regular backup

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
 
 account_sid = 'ACc58f8372eb9b75f8d5fe4d587bccd167'
@@ -12,3 +14,26 @@ message = client.messages.create(
                           )
 
 print(message.sid)
+
+def initialSMSReply():
+    # replying to message 
+    resp= MessaginResponse();
+
+    #adding message
+    resp.message("Welcome to Rutgers ChatBot! Please specify one of the following options:Events, Busses, Dining, Sports, Recreation, Places, or Schedule of Classes")
+
+    return str(resp)
+
+def branchingFromFirst():
+    # getting message user sent to our number
+    body = request.values.get('Body', None)
+
+    # initializing our Twiml response
+    resp = MessagingResponse()
+
+    # removing white space from end and making string lowercase
+    body = body.strip()
+    body = body.lower()
+
+    # going through possible cases
+
