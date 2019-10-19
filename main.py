24a4cd67d6dc7372a25ad769cf9da9b730e67ecb
Noman Ali <alinoman1999@gmail.com>
2019-10-19 15:24:54 -0400

    Figured out Parsing XML and printing details

--- main.py
+++ main.py
@@ -10,16 +10,30 @@ auth_token = '54ed90c4225c224e23f0d35568b307d8'
 
 client = Client(account_sid, auth_token)
 
-message = client.messages.create(
-                              from_='+12055129674',
-                              body='Welcome to RUHelp',
-                              to='+17326667804'
-                          )
+# message = client.messages.create(
+#                               from_='+12055129674',
+#                               body='Welcome to RUHelp',
+#                               to='+17326667804'
+#                           )
 
 # print(message.sid)
 # print(contents)
-
 print(str(len(feed['entries'])) + " events happening this week.")
 
+# for x in feed.entries:
+#     print (x.title)
+
+print('Enter interest to search for:')
+interest = input()
+
+interst = interest.lower()
+
 for x in feed.entries:
-    print(x.title)
+    event_title = x.title
+    event_title=event_title.lower()
+    if(interest in event_title):
+        print('Event found with interest:')
+        print(x.title)
+        if(x.description != x.title):
+            print(x.description)
+        print(x.event_begindatetime)
