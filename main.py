9b997f5d5809dcdadbf1677ae4bdb1d78ce6a627
Noman Ali <alinoman1999@gmail.com>
2019-10-19 13:12:20 -0400

    Added working phone numbers

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from twilio.rest import Client
 
 account_sid = 'ACc58f8372eb9b75f8d5fe4d587bccd167'
 auth_token = '54ed90c4225c224e23f0d35568b307d8'
-client = Client('account_sid','auth_token')
 
+client = Client(account_sid, auth_token)
 
-message = client.messages.create(body="Message that will be sent", from_='number from twilio', to='number receiving')
+message = client.messages.create(
+                              from_='+12055129674',
+                              body='body',
+                              to='+17326667804'
+                          )
 
 print(message.sid)
