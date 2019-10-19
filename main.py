bbc6d475b1560393a61abe30140a64cef2ff3788
Noman Ali <alinoman1999@gmail.com>
2019-10-19 13:28:28 -0400

    imported URL Lib to get requests

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from twilio.rest import Client
+import urllib.request
+
+contents = urllib.request.urlopen("http://example.com/foo/bar").read()
 
 account_sid = 'ACc58f8372eb9b75f8d5fe4d587bccd167'
 auth_token = '54ed90c4225c224e23f0d35568b307d8'
