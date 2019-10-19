7e12ead67b38ec86a2704e141b80cbb5179ee895
Noman Ali <alinoman1999@gmail.com>
2019-10-19 14:09:34 -0400

    erai

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from twilio.rest import Client
 import urllib.request
+import feedparser
 
-contents = urllib.request.urlopen("http://example.com/foo/bar").read()
+contents = urllib.request.urlopen("http://ruevents.rutgers.edu/events/getEventsRss.xml?numberOfDays=7").read()
+feed = feedparser.parse('http://ruevents.rutgers.edu/events/getEventsRss.xml?numberOfDays=7')
 
 account_sid = 'ACc58f8372eb9b75f8d5fe4d587bccd167'
 auth_token = '54ed90c4225c224e23f0d35568b307d8'
 
 client = Client(account_sid, auth_token)
 
-message = client.messages.create(
-                              from_='+12055129674',
-                              body='body',
-                              to='+17326667804'
-                          )
+# message = client.messages.create(
+#                               from_='+12055129674',
+#                               body='body',
+#                               to='+17326667804'
+#                           )
 
-print(message.sid)
-print(contents)
+# print(message.sid)
+# print(contents)
+
+print(str(len(feed['entries'])) + " events happening this week.")
+
+for x in feed.entries:
+    print(x.title)
