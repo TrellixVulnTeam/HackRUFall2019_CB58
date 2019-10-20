fd8c807d426501009406ef89a44264922bdbad04
Pranav <pbhojraj@gmail.com>
2019-10-20 00:24:14 -0400

    EEEE

--- run.py
+++ run.py
@@ -1,21 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
-@app.route("/sms", methods=['POST'])
-def sms_ahoy_reply():
-    """Respond to incoming messages with a friendly SMS."""
-    # Start our response
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Start our TwiML response
     resp = MessagingResponse()
-
     # Add a message
-    resp.message(requst.)
+    userInput=request.form['CurrentInput']
+    resp.message(str(userInput))
 
     return str(resp)
-#@app.route("/sms", methods=['GET'])
-if request.method=='POST':
-    return request.form.get('keywords')
 
 if __name__ == "__main__":
     app.run(debug=True)
